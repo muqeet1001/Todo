8cde094789c654a3286059c1b20848b7c31ad6f8
muqeet1001 <abdul1002muqeet@gmail.com>
2025-08-31 11:58:19 +0530

    Todo completed

--- src/Wrapper.jsx
+++ src/Wrapper.jsx
@@ -1,12 +1,15 @@
 import React, { createContext } from "react";
 import { useState } from "react";
 
- export const todocontext = createContext(null);
+export const todocontext = createContext(null);
 function Wrapper(props) {
-  console.log(props);
   const [todo, settodo] = useState([]);
- 
-  return  <todocontext.Provider value={[todo, settodo]}>{props.App}</todocontext.Provider>;
+
+  return (
+    <todocontext.Provider value={[todo, settodo]}>
+      {props.App}
+    </todocontext.Provider>
+  );
 }
 
 export default Wrapper;
